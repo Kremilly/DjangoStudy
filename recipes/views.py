85792cfa3b0a9884c6e1e948ddcc00fe15dae929
Kremilly <contact@kremilly.com>
2025-08-30 11:46:37 -0300

    feat: update recipe template for conditional rendering of cover image and author details; enhance views for category filtering and recipe retrieval

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
     })
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
+
