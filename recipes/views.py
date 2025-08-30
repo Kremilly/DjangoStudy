2e1d137e13a1317ad96b6d6951ab46b2b394f7e2
Kremilly <contact@kremilly.com>
2025-08-30 03:16:01 -0300

    feat: update recipes app views and templates for home, contact, and about pages

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
 
 def contact(request):
-    return HttpResponse('Contact')
+    return render(request, 'recipes/contact.html')
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'recipes/about.html')
