f2c84a761e6355692dac3f08840d5d0e8cf0131c
Kremilly <contact@kremilly.com>
2025-08-31 08:56:28 -0300

    fix: enhance home view to display message for empty recipe list; improve CSS for layout consistency

--- recipes/views.py
+++ recipes/views.py
@@ -1,35 +1,34 @@
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from utils.recipes.factory import make_recipe
-from .models import Recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-# Create your views here.
+from recipes.models import Recipe
+
+
 def home(request):
     recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes
-    })
-
-def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
         is_published=True,
     ).order_by('-id')
 
-    if not recipes:
-        raise Http404('Not found 🥲')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category '
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
-        'is_detail_page': True
+        'is_detail_page': True,
     })
-
