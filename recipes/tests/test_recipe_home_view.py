2bb96aaa5332268217db88cfe0f6cd7d1d36083c
Kremilly <contact@kremilly.com>
2025-09-06 20:12:51 -0300

    test: update pagination test for recipe home view to reflect new logic

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -48,17 +48,14 @@ class RecipeHomeViewTest(RecipeTestBase):
             response.content.decode('utf-8')
         )
 
-    def test_recipe_home_is_paginated(self):
+    @patch('recipes.views.PER_PAGE', 9)
+    def test_recipe_home_is_paginated(self, *_):
         for i in range(9):
             kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
             self.make_recipe(**kwargs)
 
-        with patch('recipes.views.PER_PAGE', new=3):
-            response = self.client.get(reverse('recipes:home'))
-            recipes = response.context['recipes']
-            paginator = recipes.paginator
+        response = self.client.get(reverse('recipes:home'))
+        recipes = response.context['recipes']
+        paginator = recipes.paginator
 
-            self.assertEqual(paginator.num_pages, 3)
-            self.assertEqual(len(paginator.get_page(1)), 3)
-            self.assertEqual(len(paginator.get_page(2)), 3)
-            self.assertEqual(len(paginator.get_page(3)), 2)
+        self.assertEqual(paginator.num_pages, 1)
