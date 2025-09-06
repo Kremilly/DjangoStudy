64a7fa7491fee7da57a1889a042e150702352446
Kremilly <contact@kremilly.com>
2025-09-06 19:42:49 -0300

    feat: implement pagination functionality with dynamic page range and improved template structure

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
-from django.http.response import Http404
+from django.core.paginator import Paginator
 from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination_range
 
 from recipes.models import Recipe
 
@@ -10,9 +12,25 @@ def home(request):
         is_published=True,
     ).order_by('-id')
 
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
+
 
 def category(request, category_id):
     recipes = get_list_or_404(
@@ -27,6 +45,7 @@ def category(request, category_id):
         'title': f'{recipes[0].category.name} - Category | '
     })
 
+
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
@@ -34,13 +53,14 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
-    
+
+
 def search(request):
     search_term = request.GET.get('q', '').strip()
 
     if not search_term:
         raise Http404()
-    
+
     recipes = Recipe.objects.filter(
         Q(
             Q(title__icontains=search_term) |
@@ -48,7 +68,7 @@ def search(request):
         ),
         is_published=True
     ).order_by('-id')
-    
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
