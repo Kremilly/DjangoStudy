e2a8960dc9d6d70666bc5d4778c8c2064a6d0bf4
Kremilly <contact@kremilly.com>
2025-09-07 12:35:31 -0300

    fix: update pagination to use PER_PAGE constant in home view

--- recipes/views.py
+++ recipes/views.py
@@ -14,8 +14,8 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-    
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
